74fdb715b3ecc44aa3f2bb9c6febd43603ab55c7
julianlee98 <julian_lee_@outlook.com>
2018-08-19 04:37:13 +1000

    Database connection complete, understanding of how firestore and cloud functions work gained

--- app/src/main/java/com/moonstone/ezmaps_app/MainPageActivity.java
+++ app/src/main/java/com/moonstone/ezmaps_app/MainPageActivity.java
@@ -6,22 +6,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainPageActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button logOutButton;
+    private Button logOutButton, databaseTestButton;
     private FirebaseAuth mAuth;
     private TextView welcomeText;
+
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
 
+        db = FirebaseFirestore.getInstance();
+
         logOutButton = findViewById(R.id.logOutButton);
         logOutButton.setOnClickListener(this);
+
+        databaseTestButton = findViewById(R.id.databaseTestButton);
+        databaseTestButton.setOnClickListener(this);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -36,11 +52,42 @@ public class MainPageActivity extends AppCompatActivity implements View.OnClickL
         startActivity(new Intent(this, UserIdentification.class));
     }
 
+    private void testDatabase(){
+        String email = mAuth.getCurrentUser().getEmail();
+        final String Uid = mAuth.getUid();
+
+        Map<String, Object> userMap = new HashMap<>();
+
+        userMap.put("email", email);
+        userMap.put("counter", 0);
+
+        db.collection("users").document(Uid).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+               Object a = documentSnapshot.get("email");
+               Object b = documentSnapshot.get("counter");
+               Toast.makeText(MainPageActivity.this, a.toString(), Toast.LENGTH_SHORT).show();
+
+               db.collection("users").document(Uid).update("counter", Integer.parseInt(b.toString()) + 1);
+            }
+        });
+
+//        db.collection("users").document(Uid).set(userMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+//            @Override
+//            public void onSuccess(Void aVoid) {
+//                Toast.makeText(MainPageActivity.this, "Database successfully changed", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+    }
+
     @Override
     public void onClick(View view){
         switch (view.getId()){
             case R.id.logOutButton:
                 userSignOut();
+                break;
+            case R.id.databaseTestButton:
+                testDatabase();
                 break;
         }
     }
