8a82e18281cd5e7a4401229ea8b4bbec85f83b4c
Will <31301775+WillsGitHubAcc@users.noreply.github.com>
2018-08-26 18:24:31 +1000

    First attempt at FCM

--- app/src/main/java/com/moonstone/ezmaps_app/MainActivity.java
+++ app/src/main/java/com/moonstone/ezmaps_app/MainActivity.java
@@ -1,5 +1,8 @@
 package com.moonstone.ezmaps_app;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,6 +25,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         viewPager = (ViewPager) findViewById(R.id.viewPager);
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationManager mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+            NotificationChannel mChannel = new NotificationChannel(Constants.CHANNEL_ID, Constants.CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+            mChannel.getDescription();
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+
+            mChannel.setVibrationPattern(new long[] {1000, 100});
+
+            mNotificationManager.createNotificationChannel(mChannel);
+        }
 
         adapter = new TabAdapter(getSupportFragmentManager());
 
---
+++ app/src/main/java/com/moonstone/ezmaps_app/MyNotificationManager.java
@@ -0,0 +1,46 @@
+package com.moonstone.ezmaps_app;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+import java.util.ConcurrentModificationException;
+
+public class MyNotificationManager {
+    private Context mCtx;
+    private static MyNotificationManager mInstance;
+
+    private MyNotificationManager (Context context){
+        mCtx = context;
+    }
+
+    public static synchronized MyNotificationManager getInstance(Context context){
+        if(mInstance == null){
+            mInstance = new MyNotificationManager(context);
+        }
+        return mInstance;
+    }
+
+    public void displayNotification(String title, String body){
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(mCtx, Constants.CHANNEL_ID)
+                .setSmallIcon(R.drawable.bell_1096280_960_720)
+                .setContentTitle(title)
+                .setContentText(body);
+
+        Intent intent = new Intent(mCtx, MainActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(mCtx, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mBuilder.setContentIntent(pendingIntent);
+
+        NotificationManager mNotificationManager = (NotificationManager) mCtx.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if(mNotificationManager != null){
+            mNotificationManager.notify(1, mBuilder.build());
+        }
+    }
+
+}
