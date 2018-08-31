f44cccffdeb700fba7127c59994a8eeb894aa4ed
julianlee98 <julian_lee_@outlook.com>
2018-08-31 22:46:39 +1000

    RecyclerViews working and understanding of tasks needed aquired

---
+++ app/src/main/java/com/moonstone/ezmaps_app/ContactRecyclerViewAdapter.java
@@ -0,0 +1,99 @@
+package com.moonstone.ezmaps_app;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ContactRecyclerViewAdapter extends RecyclerView.Adapter<ContactRecyclerViewAdapter.ViewHolder> {
+
+    private ArrayList<String> contactNames = new ArrayList<>();
+    private ArrayList<String> profilePics = new ArrayList<>();
+    private Context mContext;
+
+    //Never rendered but information is held here
+    private ArrayList<String> ids = new ArrayList<>();
+    private ArrayList<String> emails = new ArrayList<>();
+
+    public ContactRecyclerViewAdapter(Context context, ArrayList<String> contactNames, ArrayList<String> profilePics, ArrayList<String> ids, ArrayList<String> emails){
+        this.contactNames = contactNames;
+        this.profilePics = profilePics;
+        this.mContext = context;
+        this.ids = ids;
+        this.emails = emails;
+    }
+
+    //Actually recycles the view holders
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.contactlistitem, viewGroup, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    //Called every time a new item is added to the list
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int i) {
+        //Gets the image and puts it into the referenced imageView
+        Glide.with(mContext).asBitmap().load(profilePics.get(i)).into(viewHolder.profilePic);
+
+        viewHolder.contactName.setText(contactNames.get(i));
+
+        //Add onclicklistener to each list entry
+        viewHolder.ContactParentLayout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Toast.makeText(mContext,contactNames.get(i), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        viewHolder.id = ids.get(i);
+        viewHolder.email = emails.get(i);
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return contactNames.size();
+    }
+
+    //Basically the class of the entry itself
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        CircleImageView profilePic;
+        TextView contactName;
+        RelativeLayout ContactParentLayout;
+        String email;
+        String id;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            profilePic = itemView.findViewById(R.id.profilePic);
+            contactName = itemView.findViewById(R.id.contactName);
+            ContactParentLayout = itemView.findViewById(R.id.contactParentLayout);
+
+        }
+    }
+
+    public void filterList(ArrayList<String> contactNames, ArrayList<String> profilePics, ArrayList<String> ids, ArrayList<String> emails){
+        this.contactNames = contactNames;
+        this.profilePics = profilePics;
+        this.ids = ids;
+        this.emails = emails;
+        notifyDataSetChanged();
+    }
+}
