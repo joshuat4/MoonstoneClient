c9ab14eb4d899622cfb4df1766a96bdf24cc23a1
julianlee98 <julian_lee_@outlook.com>
2018-08-31 22:46:39 +1000

    RecyclerViews working and understanding of tasks needed aquired

--- app/src/main/java/com/moonstone/ezmaps_app/Tab3Fragment.java
+++ app/src/main/java/com/moonstone/ezmaps_app/Tab3Fragment.java
@@ -2,14 +2,126 @@ package com.moonstone.ezmaps_app;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Document;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Tab3Fragment extends Fragment {
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+    private View fragmentLayout;
+    private ContactRecyclerViewAdapter adapter;
+
+    private EditText contactFiler;
+    private Button newContactButton;
+
+    //Arrays needed for recyclerView
+    private ArrayList<String> profilePics = new ArrayList<>() ;
+    private ArrayList<String> ids = new ArrayList<>();
+    private ArrayList<String> emails = new ArrayList<>();
+    private ArrayList<String> names = new ArrayList<>();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_three, container, false);
+        fragmentLayout = inflater.inflate(R.layout.fragment_three, container, false);
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        contactFiler = fragmentLayout.findViewById(R.id.contactFilter);
+        newContactButton = fragmentLayout.findViewById(R.id.contactAddButton);
+
+        Task<QuerySnapshot> d = db.collection("users").get();
+
+        //Filter code
+        contactFiler.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                filter(s.toString());
+            }
+        });
+
+        d.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                List<DocumentSnapshot> l = task.getResult().getDocuments();
+                //Fill in the necessary arrays
+                for (DocumentSnapshot doc : l) {
+                    profilePics.add(doc.get("profilePic").toString());
+                    emails.add(doc.get("email").toString());
+                    names.add(doc.get("name").toString());
+                    ids.add(doc.getId());
+                }
+                initRecyclerView();
+            }
+        });
+        return fragmentLayout;
+    }
+
+    //(Context context, ArrayList<String> contactNames, ArrayList<String> profilePics, ArrayList<String> ids, ArrayList<String> emails){
+
+    //Sets up the recycler view
+    private void initRecyclerView(){
+        RecyclerView recyclerView =  fragmentLayout.findViewById(R.id.contactRecyclerView);
+        adapter = new ContactRecyclerViewAdapter(getActivity(), names, profilePics, ids, emails);
+        recyclerView.setAdapter(adapter) ;
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    private void filter(String text){
+
+        //Filtered arrays
+        ArrayList<String> fprofilePics = new ArrayList<>() ;
+        ArrayList<String> fids = new ArrayList<>();
+        ArrayList<String> femails = new ArrayList<>();
+        ArrayList<String> fnames = new ArrayList<>();
+
+        int counter = 0;
+
+        for(String name : names){
+            if(name.toLowerCase().contains(text.toLowerCase())){
+                fprofilePics.add(profilePics.get(counter));
+                fids.add(ids.get(counter));
+                femails.add(emails.get(counter));
+                fnames.add(names.get(counter));
+            }
+            counter += 1;
+        }
+        adapter.filterList(fnames, fprofilePics, fids, femails);
     }
 }
