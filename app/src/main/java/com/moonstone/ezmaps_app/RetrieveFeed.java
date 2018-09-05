af2adc73bd08cbc338eb3fd94ce7922e622e8c18
Hon Khay Kevin Cheong <h.cheong2@student.unimelb.edu.au>
2018-09-05 21:53:44 +1000

    all files

--- app/src/main/java/com/moonstone/ezmaps_app/RetrieveFeed.java
+++ app/src/main/java/com/moonstone/ezmaps_app/RetrieveFeed.java
@@ -1,20 +1,36 @@
+package com.moonstone.ezmaps_app;
+
 import android.os.AsyncTask;
+import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class RetrieveFeed extends AsyncTask<String, Void, JSONObject> {
+public class RetrieveFeed extends AsyncTask<String, Void, JSONArray> {
     private Exception exception;
 
-    @Override
-    protected JSONObject doInBackground(String...urlString){
+    public interface AsyncResponse {
+        void processFinish(JSONArray output);
+    }
+
+    public AsyncResponse delegate = null;
+
+    public RetrieveFeed(AsyncResponse delegate){
+        this.delegate = delegate;
+    }
+
+
+
+    protected JSONArray doInBackground(String...urlString){
 
         try {
             HttpURLConnection urlConnection = null;
@@ -26,18 +42,16 @@ public class RetrieveFeed extends AsyncTask<String, Void, JSONObject> {
             urlConnection.setConnectTimeout(15000 /* milliseconds */);
             urlConnection.setDoOutput(true);
             urlConnection.connect();
-
             BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
             StringBuilder sb = new StringBuilder();
-
             String line;
             while ((line = br.readLine()) != null) {
                 sb.append(line + "\n");
             }
             br.close();
             String jsonString = sb.toString();
-            System.out.println("JSON: " + jsonString);
-            JSONObject reply = new JSONObject(jsonString);
+//            System.out.println("JSON: " + jsonString);
+            JSONArray reply = new JSONArray(jsonString);
             return reply;
         }
         catch (Exception e){
@@ -45,4 +59,10 @@ public class RetrieveFeed extends AsyncTask<String, Void, JSONObject> {
             return null;
         }
     }
+
+
+    protected void onPostExecute(JSONArray reply) {
+        delegate.processFinish(reply);
+    }
+
 }
--- app/src/main/java/com/moonstone/ezmaps_app/ezdirection.java
+++ app/src/main/java/com/moonstone/ezmaps_app/ezdirection.java
@@ -1,29 +1,70 @@
 package com.moonstone.ezmaps_app;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
-public class ezdirection extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+
+public class ezdirection extends AppCompatActivity implements RetrieveFeed.AsyncResponse{
+    private ArrayList<String> imageUrlsList;
+    private ArrayList<String> textDirectionsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ezdirection);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //get search address from search bar
+        Intent intent = getIntent();
+        String destination = intent.getStringExtra("destination");
+
+        //execute async task
+        new RetrieveFeed(this).execute("https://us-central1-it-project-moonstone-43019.cloudfunctions.net/mapRequest?text=145%20Queensberry%20Street,%20Carlton%20VIC---Student%20Village,%20The%20University%20of%20Melbourne%20Campus,%20Leicester%20Street,%20Carlton%20VIC");
+
     }
+
+    @Override
+    public void processFinish(JSONArray output){
+        //Here you will receive the result fired from async class
+        //of onPostExecute(result) method.;
+        imageUrlsList = new ArrayList<>();
+        textDirectionsList = new ArrayList<>();
+        for(int i=0; i<output.length(); i ++){
+            try {
+                JSONObject object = output.getJSONObject(i);
+                imageUrlsList.add(object.getString("imageURL"));
+                textDirectionsList.add(object.getString("description"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        initRecyclerView();
+    }
+
+
+
+
+
+    private void initRecyclerView() {
+        final String TAG = "initRecyclerView";
+        Log.d(TAG, "initRecyclerView: init recyclerview");
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(layoutManager);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(textDirectionsList, imageUrlsList, this);
+        recyclerView.setAdapter(adapter);
+    }
+
 
 }
