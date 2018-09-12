7cdbbd3babcba0786be1e0b0f920a7fdd561b544
Kenny <kennylee@Kennys-iMac.local>
2018-09-12 16:29:05 +1000

    added catch null pointer val

--- app/src/main/java/com/moonstone/ezmaps_app/Tab3Fragment.java
+++ app/src/main/java/com/moonstone/ezmaps_app/Tab3Fragment.java
@@ -99,9 +99,11 @@ public class Tab3Fragment extends Fragment {
             db.collection("users").document(Uid).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                 @Override
                 public void onSuccess(DocumentSnapshot documentSnapshot) {
-                final ArrayList<String> contacts = (ArrayList<String>) documentSnapshot.get("contacts");
 
-                if(!contacts.isEmpty()){
+
+                try {
+                    final ArrayList<String> contacts = (ArrayList<String>) documentSnapshot.get("contacts");
+
                     for (String contact : contacts){
                         db.collection("users").document(contact).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                             @Override
@@ -121,10 +123,11 @@ public class Tab3Fragment extends Fragment {
                         });
                     }
 
-                }else{
+                } catch (NullPointerException e) {
                     contactsLoading.setVisibility(View.GONE);
                     contactsAvailable = false;
                 }
+
 
                 }
             });
