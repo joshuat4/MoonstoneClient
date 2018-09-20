57657686ba360913b7777b31cebb6ec6147b0a7d
Kenny Lee <kenny_seven@icloud.com>
2018-09-20 23:17:40 +1000

    added testing account for quick access to app

--- app/src/main/java/com/moonstone/ezmaps_app/FrontPage.java
+++ app/src/main/java/com/moonstone/ezmaps_app/FrontPage.java
@@ -1,13 +1,22 @@
 package com.moonstone.ezmaps_app;
 
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 
 import butterknife.ButterKnife;
 import butterknife.BindView;
@@ -18,11 +27,36 @@ public class FrontPage extends AppCompatActivity {
     @BindView(R.id.loginButton) Button _loginButton;
     @BindView(R.id.signUpButton) Button _signupButton;
 
+    /* DELETE HERE AFTER TESTING */
+    @BindView(R.id.test1) Button _test1;
+    @BindView(R.id.test2) Button _test2;
+    private FirebaseAuth mAuth;
+    /* DELETE HERE AFTER TESTING */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front_page);
         ButterKnife.bind(this);
+
+
+        /* DELETE HERE AFTER TESTING */
+        mAuth = FirebaseAuth.getInstance();
+        _test1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userLogin("test@test.com","testing");
+            }
+        });
+
+        _test2.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userLogin("kenny@lol.com","aloha");
+
+            }
+        });
+        /* DELETE HERE AFTER TESTING */
 
         _signupButton.setOnClickListener(new OnClickListener() {
             @Override
@@ -42,6 +76,35 @@ public class FrontPage extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void userLogin(String email, String password){
+
+        // Authenticate the user
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Intent intent = new Intent(FrontPage.this, MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+
+
+                        } else {
+
+                            if(task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                Toast.makeText(getApplicationContext(), "Invalid login. Please try again.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Toast.makeText(getApplicationContext(), task.getException().getMessage(),
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+
+                });
     }
 
 
