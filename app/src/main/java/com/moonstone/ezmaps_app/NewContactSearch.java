86c5af0ac47827fa9ccfdd5ee2d6d063e52d0748
Kenny Lee <kennyl2@student.unimelb.edu.au>
2018-09-24 21:12:12 +1000

    keyboard, focus drop at add contacts

--- app/src/main/java/com/moonstone/ezmaps_app/NewContactSearch.java
+++ app/src/main/java/com/moonstone/ezmaps_app/NewContactSearch.java
@@ -1,5 +1,7 @@
 package com.moonstone.ezmaps_app;
 
+import android.content.Context;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
@@ -9,7 +11,9 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 
@@ -150,6 +154,23 @@ public class NewContactSearch extends AppCompatActivity{
             counter += 1;
         }
         adapter.filterList(fnames, fprofilePics, fids, femails);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if ( v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent( event );
     }
 
 }
