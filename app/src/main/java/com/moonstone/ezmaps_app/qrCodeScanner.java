058962f66ef4416e0fad138060875a683ad57a33
Josh <joshuat4@student.unimelb.edu.au>
2018-10-11 06:00:19 +1100

    Building QR reader.

--- app/src/main/java/com/moonstone/ezmaps_app/qrCodeScanner.java
+++ app/src/main/java/com/moonstone/ezmaps_app/qrCodeScanner.java
@@ -7,7 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+
 public class qrCodeScanner extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
